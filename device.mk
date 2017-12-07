89fe7af2b9eb9901c9ba63f389c781fa65bbc929
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    phoenix: Build dummy android.hidl.base@1.0
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor \
-    libhidltransport
+    libhidltransport \
+    android.hidl.base@1.0
 
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
