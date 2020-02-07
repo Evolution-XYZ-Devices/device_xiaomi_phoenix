94de40b215795a504a1073700602f3ad345b779c
Alastair Donaldson <afdx@google.com>
2020-02-07 10:28:40 +0000

    phoenix: Enable vulkan dEQP level 2020-03-01
    This change sets the new Vulkan dEQP level feature flag to the value
    corresponding to the Vulkan dEQP tests for Android 11.
    
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Change-Id: I7906428e2659bcfec25dbfac28641b0595a33419

--- device.mk
+++ device.mk
@@ -75,7 +75,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
 
-# fastbootd
+# Display/Graphics
+PRODUCT_COPY_FILES += \
+     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.vulkan.deqp.level.xml
+
+# Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
 
