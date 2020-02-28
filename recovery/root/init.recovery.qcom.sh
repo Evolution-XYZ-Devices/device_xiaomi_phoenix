37290d717336458aca80a02da7604e8a09bd4dcd
PeterCxy <peter@typeblog.net>
2020-02-28 16:40:22 +0100

    phoenix: Add Recovery support
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@gmail.com>
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- recovery/root/init.recovery.qcom.sh
+++ recovery/root/init.recovery.qcom.sh
@@ -8,4 +8,3 @@ sleep 5
 # (vendor and system should be more than enough)
 ln -sf /dev/block/mapper/system /dev/block/bootdevice/by-name/system
 ln -sf /dev/block/mapper/vendor /dev/block/bootdevice/by-name/vendor
-
