9f8ff71cc89432c25a8ad27f3888b5afe4534d6a
Peter Cai <peter@typeblog.net>
2020-02-29 19:53:27 +0800

    phoenix: parts: allow switching notch style
    * Some may actually like the non-filled punchhole * 2 notch.
    * Removed toast message
    * Bring it back for OSS builds
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- device.mk
+++ device.mk
@@ -363,6 +363,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/com.nxp.mifare.xml
 
+# Notch style overlay
+PRODUCT_PACKAGES += \
+    NotchNoFillOverlay
+
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayPhoenix \
