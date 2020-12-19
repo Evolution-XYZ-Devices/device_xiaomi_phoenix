d5272cc9543fcba6329029b80ad45af877c6e9d2
chiru2000 <chiranthangadi123@gmail.com>
2020-12-19 16:07:08 +0000

    phoenix: Switch to AOSP BT stack
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -40,3 +40,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.ril.disable.power.collapse=0 \
     power.saving.mode=1 \
     pm.sleep_mode=1
+
+# Audio
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bluetooth.a2dp_offload.supported=false \
+    persist.bluetooth.a2dp_offload.disabled=true \
+    persist.bluetooth.bluetooth_audio_hal.disabled=true \
+    vendor.audio.feature.a2dp_offload.enable=false \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=false
+
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.bluetooth.library_name=libbluetooth.so
