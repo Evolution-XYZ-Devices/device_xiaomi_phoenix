fc8767b3c8016eaaef1e07d204ec6eafae0c9402
TheScarastic <warabhishek@gmail.com>
2021-01-19 05:50:34 +0100

    phoenix: Override citsensorservice and start it on late_start
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default \
     $(LOCAL_PATH)/rootdir/etc/fstab.emmc:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
 
+# Init
+PRODUCT_PACKAGES += \
+    init.qcom.rc
+
 # IPACM
 PRODUCT_PACKAGES += \
     android.hardware.tetheroffload.config@1.0
