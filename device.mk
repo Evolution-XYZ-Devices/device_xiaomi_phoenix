3f7e5a15fcf65dfebf7026e0bd4433be87d5a431
ugly-kid-af <dwarmachine24@gmail.com>
2021-02-10 11:25:12 +0530

    phoenix: import missing sm6150-idp-snd-card_Button_Jack.kl
    *Fixes earphone/headset volume buttons
    
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>
    Change-Id: 8bcf017b2cacc276757de5b0975b2b9e1ceb9f98

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ PRODUCT_PACKAGES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
+    $(LOCAL_PATH)/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
 
 # Lights
 PRODUCT_PACKAGES += \
