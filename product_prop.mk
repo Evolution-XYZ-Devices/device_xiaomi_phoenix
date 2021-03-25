bibindasb/device-xiaomi-phoenix-cherish

phoenix: Use phase offsets as duration
In Android R, the SurfaceFlinger calculate phase offsets
based on duration. Adopt the new way of specifying
phase offsets to SurfaceFlinger.

Bug: 158464992
Test: scrolling, app launching/closing, playing video
Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415
Signed-off-by: karthik558 <karthik.lal558@gmail.com>
Signed-off-by: chiranth <chiranthangadi123@gmail.com>

product_prop.mk
@@ -56,3 +56,18 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth.so
+
+# Enable app/sf phase offset as durations. The numbers below are translated from the existing
+# positive offsets by finding the duration app/sf will have with the offsets.
+# For SF the previous value was 6ms which under 16.6ms vsync time (60Hz) will leave SF with ~10.5ms
+# for each frame. For App the previous value was 2ms which under 16.6ms vsync time will leave the
+# App with ~20.5ms (16.6ms * 2 - 10.5ms - 2ms). The other values were calculated similarly.
+# Full comparison between the old vs. the new values are captured in
+# https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.use_phase_offsets_as_durations=1
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.late.sf.duration=10500000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.late.app.duration=20500000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.early.sf.duration=21000000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.early.app.duration=16500000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.earlyGl.sf.duration=13500000
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += debug.sf.earlyGl.app.duration=21000000

Evolution-XYZ-Devices/device_xiaomi_phoenix

Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

product_prop.mk
@@ -52,3 +52,18 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Bluetooth
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth.so
+
+# Enable app/sf phase offset as durations. The numbers below are translated from the existing
+# positive offsets by finding the duration app/sf will have with the offsets.
+# For SF the previous value was 6ms which under 16.6ms vsync time (60Hz) will leave SF with ~10.5ms
+# for each frame. For App the previous value was 2ms which under 16.6ms vsync time will leave the
+# App with ~20.5ms (16.6ms * 2 - 10.5ms - 2ms). The other values were calculated similarly.
+# Full comparison between the old vs. the new values are captured in
+# https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.use_phase_offsets_as_durations=1
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.late.sf.duration=10500000
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.late.app.duration=20500000
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.early.sf.duration=21000000
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.early.app.duration=16500000
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.earlyGl.sf.duration=13500000
+PRODUCT_PROPERTY_OVERRIDES += debug.sf.earlyGl.app.duration=21000000