5b560d9b5b640f23931446c5298bc5021470c11e
LuK1337 <priv.luk@gmail.com>
2021-03-30 12:26:59 +0530

    phoenix: lights: Drop Custom light HAL
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- device.mk
+++ device.mk
@@ -111,10 +111,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
     $(LOCAL_PATH)/keylayout/sm6150-idp-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sm6150-idp-snd-card_Button_Jack.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_phoenix
-
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
