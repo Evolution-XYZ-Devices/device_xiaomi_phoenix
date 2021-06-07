94e4dd6676ede39852a83db7ba60217d16f76dc6
chiru2000 <chiranthangadi123@gmail.com>
2021-06-07 23:28:46 +0530

    phoenix: init: Update redfin FP to RQ3A.210605.005
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- init/init_phoenix.cpp
+++ init/init_phoenix.cpp
@@ -60,13 +60,13 @@ constexpr const char *DEVICES[] = {
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "redfin-user 11 RQ2A.210505.003 7255357 release-keys",
-    "redfin-user 11 RQ2A.210505.003 7255357 release-keys",
+    "redfin-user 11 RQ3A.210605.005 7349499 release-keys",
+    "redfin-user 11 RQ3A.210605.005 7349499 release-keys",
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys",
-    "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys",
+    "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys",
+    "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys",
 };
 
 constexpr const char *CLIENT_ID[] = {
