9dacc115c81b2c6dd2131f6e23772174686df8bf
ghostrider-reborn <adithya.r02@outlook.com>
2021-06-13 18:27:04 +0530

    phoenix: Configure cpusets for dex2oat
     * use all 8 cpus for all operations
     * https://source.android.com/devices/tech/dalvik/configure
    
    Change-Id: I202e425b2aa752b9e6115938bff6051907c8ee57
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: chiru2000 <chiranthangadi123@gmail.com>

--- device.mk
+++ device.mk
@@ -163,6 +163,15 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# Configure dex2oat
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.dex2oat-threads=8 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true
