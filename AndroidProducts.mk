7678d6825bf064dd49862c7d5eddd139581a479f
RonitKrShah <ronitkrshah@tuta.io>
2022-10-11 13:11:09 +0530

    phoenix: Pixel-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_phoenix.mk
+    $(LOCAL_DIR)/aosp_phoenix.mk
--- aosp_phoenix.mk
+++ aosp_phoenix.mk
@@ -1,23 +1,22 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_phoenix
+PRODUCT_NAME := aosp_phoenix
 PRODUCT_DEVICE := phoenix
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X2
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
 
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Sreekanth
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
