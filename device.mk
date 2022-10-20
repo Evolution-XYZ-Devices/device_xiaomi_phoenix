986694b36a80ac7650439d65817f1008d494f074
Shripal Jain <shripal17@gmail.com>
2022-10-20 16:44:47 +0530

    phoenix: Move processor model overrde to system_ext.prop
    Signed-off-by: Shripal Jain <shripal17@gmail.com>

--- device.mk
+++ device.mk
@@ -116,10 +116,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-# CPU
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.processor.model = SD730
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
