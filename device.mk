001d5c650a77cb61df763fb08f737d201cbe06d0
Jay Mistry <jaymistry258@gmail.com>
2023-10-15 03:14:09 +0000

    phoenix: Drop Xiomiparts
    Signed-off-by: Jay Mistry <jaymistry258@gmail.com>

--- device.mk
+++ device.mk
@@ -143,8 +143,8 @@ PRODUCT_PACKAGES += \
     services-ext
 
 # Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+#PRODUCT_PACKAGES += \
+#    XiaomiParts
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-parts.xml
